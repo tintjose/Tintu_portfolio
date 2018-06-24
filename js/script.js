a699b7d0e61a4529244913794cbb482b85141150
Tintu Jose <tintjose@gmail.com>
2018-06-24 01:59:47 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $('#slides').superslides({
+    animation:'fade',play:5000,
+    pagination:false
+  });
+var typed=new Typed(".typed",{
+  strings: ["Software Engineer..", "Full Stack Developer..", "Web Developer..","Recent Graduate Student.."],
+  typeSpeed: 80,
+  loop: true,
+  startDelay:1000,
+  showCursor:false
+
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+});
+
+
+       var skillsTopOffset=$(".skillssection").offset().top;
+       $(window).scroll(function(){
+         if(window.pageYOffset>skillsTopOffset-$(window).height()+200){
+           $('.chart').easyPieChart({
+                     easing:'easeInOut',
+                     barColor:'#fff',
+                     trackColor:false,
+                     scaleColor: false,
+                     lineWidth: 4,
+                     size:152,
+                     onStep: function(from, to, percent){
+                       $(this.el).find('.percent').text(Math.round(percent));
+                     }
+
+                  });
+         }
+       });
+});
