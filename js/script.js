4272528037ed5f21a8a487ebfa06b96e406e86cc
Tintu Jose <tintjose@gmail.com>
2018-06-24 16:00:55 -0700

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// $(window).on("load",function(){
+//   $(".loader").fadeOut(500);
+// })
+
+
 $(document).ready(function(){
   $('#slides').superslides({
     animation:'fade',play:5000,
@@ -11,6 +16,17 @@ var typed=new Typed(".typed",{
   showCursor:false
 
 });
+$("down").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 $('.owl-carousel').owlCarousel({
     loop:true,
     items:4,
@@ -48,4 +64,38 @@ $('.owl-carousel').owlCarousel({
                   });
          }
        });
+
+$("#navigation li a").click(function(e){
+  e.preventDefault();
+  var targetElement=$(this).attr("href");
+  var targetPosition=$(targetElement).offset().top;
+  $("html,body").animate({scrollTop:targetPosition-50},"slow");
+});
+
+
+
+       const nav = $("#navigation");
+   	const navTop = nav.offset().top;
+
+   	$(window).on("scroll", stickyNavigation);
+
+   	function stickyNavigation() {
+
+   		var body = $("body");
+
+   		if($(window).scrollTop() >= navTop) {
+        body.css("padding-top",nav.outerHeight()+"px");
+   			body.addClass("fixedNav");
+   		}
+   		else {
+        body.css("padding-top",0);
+
+   			body.removeClass("fixedNav");
+   		}
+
+
+
+
+   	}
+
 });
